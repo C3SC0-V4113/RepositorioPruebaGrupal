52ca59328791ad35535fa1e8ceb26575e1398d21
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-03-15 18:32:32 -0600

    Creacion de nuevo Fomulario
    Formulario 2 con boton

--- PruebaRep/PruebaRep/Form1.Designer.cs
+++ PruebaRep/PruebaRep/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_paramiform = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_paramiform
+            // 
+            this.btn_paramiform.Location = new System.Drawing.Point(13, 13);
+            this.btn_paramiform.Name = "btn_paramiform";
+            this.btn_paramiform.Size = new System.Drawing.Size(75, 23);
+            this.btn_paramiform.TabIndex = 0;
+            this.btn_paramiform.Text = "form2";
+            this.btn_paramiform.UseVisualStyleBackColor = true;
+            this.btn_paramiform.Click += new System.EventHandler(this.btn_paramiform_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_paramiform);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_paramiform;
     }
 }
 
