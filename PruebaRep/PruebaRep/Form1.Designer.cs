78791baf44562b3d039676766412a4f800a9571e
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-03-17 17:57:44 -0600

    Cambie de nombre el Label

--- PruebaRep/PruebaRep/Form1.Designer.cs
+++ PruebaRep/PruebaRep/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btn_paramiform = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_papyrus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -53,15 +53,15 @@
             this.btn_paramiform.UseVisualStyleBackColor = true;
             this.btn_paramiform.Click += new System.EventHandler(this.btn_paramiform_Click);
             // 
-            // label2
+            // lbl_papyrus
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(9, 179);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(348, 21);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "AQUI PROGRAMANDO O QUE HACE?";
+            this.lbl_papyrus.AutoSize = true;
+            this.lbl_papyrus.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_papyrus.Location = new System.Drawing.Point(9, 179);
+            this.lbl_papyrus.Name = "lbl_papyrus";
+            this.lbl_papyrus.Size = new System.Drawing.Size(348, 21);
+            this.lbl_papyrus.TabIndex = 1;
+            this.lbl_papyrus.Text = "AQUI PROGRAMANDO O QUE HACE?";
             // 
             // Form1
             // 
@@ -69,7 +69,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(363, 307);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_papyrus);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_paramiform);
             this.Name = "Form1";
@@ -84,7 +84,7 @@
         private System.Windows.Forms.Label label1;
 
        private System.Windows.Forms.Button btn_paramiform;
-       private System.Windows.Forms.Label label2;
+       private System.Windows.Forms.Label lbl_papyrus;
     }
 }
 
